3502d93ee5ac484ae374b9711d7d92042acffbb0
Wiliam Feijó Barzon <wiliamfeijo@gmail.com>
2022-07-31 04:37:46 +0000

    Teste

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sapui5trainningprj/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapui5_trainning_prj/test/unit/controller/SAPUI5_Trainning_VIEW.controller"
+], function () {
+	"use strict";
+});
